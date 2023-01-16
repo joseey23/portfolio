735c16e181e0b8ccc0c011dc8d1af7d6f21706a0
joseey23 <josephmaruria@gmail.com>
2023-01-16 14:49:54 +0300

    Joseey's Portfolio

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="https://drive.google.com/file/d/1JQ6OL28KZ2Ww1Y6q8sxWAsQmxIzTYQof/view?usp=sharing" target="blank"
                 >
-                  Here's my Resume
+                  My Resume
                 </a>
               </li>
             </ul>
